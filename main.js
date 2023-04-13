bec876fce79dd224f1f54ad37818c6e0e3ce3038
rgb-katana <komarova1610@yandex.ru>
2023-04-13 21:50:53 +0300

    add prettyPrint

--- main.js
+++ main.js
@@ -39,3 +39,19 @@ function sortedArrayToBST(arr, start, end) {
 }
 
 const arr1 = [1, 2, 3, 4, 5, 6, 7];
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const rootNode = sortedArrayToBST(arr1, 0, arr1.length - 1);
+prettyPrint(rootNode);
